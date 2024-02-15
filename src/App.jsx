341cdc566917a1b6fee5d7dba4e4c981d0a5d764
MHShihan <mahmodul.no.1@gmail.com>
2024-02-15 09:28:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold text-gray-900 bg-purple-400 py-4 ">
+        Redux
+      </h1>
+    </>
+  );
+}
+
+export default App;
